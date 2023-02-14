6d79575644152ec22d9af39eaa69400fd2fa9f31
atharva360 <68213646+atharva360@users.noreply.github.com>
2023-02-14 17:04:35 +0530

    Update sma.py

--- sma.py
+++ sma.py
@@ -7,14 +7,19 @@ import datetime
 import plotly.express as px
 import plotly.graph_objs as go
 from plotly.subplots import make_subplots
+import stockstats
 
 def sMA(df,ticker):
     m1 = st.number_input('Enter Moving Average 1:- ',value=50)
     m2 = st.number_input('Enter Moving Average 2:- ',value=200)
-    df['ma_1'] = df['Adj Close'].rolling(m1).mean()
-    df['ma_2'] = df['Adj Close'].rolling(m2).mean()
+    # df['ma_1'] = df['Adj Close'].rolling(m1).mean()
+    # df['ma_2'] = df['Adj Close'].rolling(m2).mean()
+    stock = stockstats.StockDataFrame.retype(df)
+    # Calculate 13-day and 50-day SMA
+    df["ma_1"] = stock.get(f"close_{m1}_sma")
+    df["ma_2"] = stock.get(f"close_{m2}_sma")
     df = df.dropna()
-    df = df[['Adj Close', 'ma_1', 'ma_2']]
+    df = df[['adj close', 'ma_1', 'ma_2']]
     Buy = []
     Sell = []
     buyprices = []
@@ -24,31 +29,36 @@ def sMA(df,ticker):
         if df.ma_1.iloc[i] > df.ma_2.iloc[i] \
         and df.ma_1.iloc[i-1] < df.ma_2.iloc[i-1]:
             Buy.append(i)
-            buyprices.append(df.iloc[i]['Adj Close'])
+            buyprices.append(df.iloc[i]['adj close'])
         elif df.ma_1.iloc[i] < df.ma_2.iloc[i] \
         and df.ma_1.iloc[i-1] > df.ma_2.iloc[i-1]:
             Sell.append(i)
-            sellprices.append(df.iloc[i]['Adj Close'])
+            sellprices.append(df.iloc[i]['adj close'])
     
-    
-
+    if Buy[0] > Sell[0]:
+        Sell.pop(0)
+        sellprices.pop(0)
+    if Buy[len(Buy)-1] > Sell[len(Sell)-1]:
+        Buy.pop()
+        buyprices.pop
+        
     fig = make_subplots(rows=1, cols=1)
     
-    fig.add_trace(go.Scatter(x=df.index, y=df['Adj Close'],name='Adj Close Price'), row=1, col=1)
+    fig.add_trace(go.Scatter(x=df.index, y=df['adj close'],name='adj close Price'), row=1, col=1)
     fig.add_trace(go.Scatter(x=df.index, y=df['ma_1'], name=f'MA{m1}'), row=1, col=1)
     fig.add_trace(go.Scatter(x=df.index, y=df['ma_2'], name=f'MA {m2}'), row=1, col=1)
     
     fig.add_trace(go.Scatter(x=df.iloc[Buy].index, 
-                             y=df.iloc[Buy]['Adj Close'], name='Buy Signal', mode='markers',
+                             y=df.iloc[Buy]['adj close'], name='Buy Signal', mode='markers',
                             marker=dict(color='green', size=8, symbol='triangle-up-dot')))
     
     fig.add_trace(go.Scatter(x=df.iloc[Sell].index, 
-                             y=df.iloc[Sell]['Adj Close'], name='Sell Signal', mode='markers',
+                             y=df.iloc[Sell]['adj close'], name='Sell Signal', mode='markers',
                             marker=dict(color='red', size=8, symbol='triangle-down-dot')))
     
     fig.update_layout(title=f"{ ticker } stock SMA Crossover strategy",
                   xaxis_title='Timeframe',
-                  yaxis_title='Adj Close Price')
+                  yaxis_title='adj close Price')
            
     st.plotly_chart(fig,use_container_width=True)
     
@@ -70,14 +80,16 @@ user_input = st.text_input('Enter The Stock Ticker', 'TATAPOWER.NS')
 
 if st.button('Intraday'):
     df = yf.download(tickers=user_input, period='1d', interval='1m')
-
+    sMA(df,user_input)
+    
+elif st.button('1Y'):
+    df = yf.download(user_input, period='1y', interval='1d')
+    sMA(df,user_input)
 else:
     start = st.date_input('Enter Start date',datetime.date(2019,1, 1))
     end = st.date_input('Enter End date',datetime.date(2023,1, 1))
     df = yf.download(user_input, start=start, end=end)
-    
-sMA(df,user_input)
-
+    sMA(df,user_input)
 
 
 
